b10632f610cbb43a41edbe27f95defcbf003ed76
priyanshu-v33 <butkii099@gmail.com>
2026-06-03 19:30:43 +0530

    updated UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
-import HomeArcade from '../components/Home';
+import HomeArcade from '@/components/Home';
 import SmoothFollower from '@/components/SmoothFollower';
-// import FooterPanel from '../components/FooterPanel';
 
 export default function RootPage(): React.JSX.Element {
   return (
     <>
       <HomeArcade />
       <SmoothFollower /> 
-{/*       
-      <FooterPanel /> */}
     </>
   );
 }
